aea63ba2f50eb3a5b79380fae1e4fda5037a9756
KojotMaciek <maciej.wilkosz@gmail.com>
2014-12-14 16:06:50 +0100

    Several instructions grouped in a block + my question in line 3

---
+++ Book_4/book4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() //DLACZEGO TU JEST INT MAIN() A NIE SAMO MAIN()??
+{
+	int wys, punkty_karne;			//definicja dwoch zmiennych typu int
+									//obie sa tego samego typu wiec
+									//wystarczy przecinek odzielajacy nazwy
+	std::cout << "Na jakiej wysokosci lecimy? [w metrach]: ";
+	std::cin >> wys;
+
+	//-----------------rozwazmy sytuacje-----------------
+	if(wys < 500)
+	{
+		std::cout << "\n" << wys << " metrow to za nisko!\n";
+		punkty_karne = 1;
+	}
+	else
+	{
+		std::cout << "\nNa wysokosci " << wys << " metrow jestes juz bezpieczny \n";
+		punkty_karne = 0;
+	}
+	//-----------------ocena twoich wynikow--------------
+	std::cout << "Masz " << punkty_karne << " punktow karnych \n";
+	if(punkty_karne) std::cout << "Popraw sie!";
+}
