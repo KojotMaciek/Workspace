7c3bf38155244c5e7f42beacd4f908e089973c13
KojotMaciek <maciej.wilkosz@gmail.com>
2014-12-16 18:59:50 +0100

    Loop for

---
+++ Book_7/book7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Stewardzie, ilu leci pasazerow ? ";
+
+	int ile;			//liczba pasazerow
+	std::cin >> ile;
+
+	//int i;				//licznik obiegow petli
+	//for(i = 1; i <= ile; i = i + 1)
+	for(int i = 1; i <= ile; i = i + 1)
+	{
+		std::cout << "Pasazer nr " << i << " prosze zapisac pasy! \n";
+	}
+	std::cout << "Skoro wszyscy juz zapieli, to ladujemy.";
+}
