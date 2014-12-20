14b456690a01e00fcefe70c3164f50a6f5e34539
KojotMaciek <maciej.wilkosz@gmail.com>
2014-12-20 15:19:15 +0100

    Optimization

--- Exercise_2/exercise2.cpp
+++ Exercise_2/exercise2.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
 	int	cale;					// wprowadzamy cale
 	double	centymetry;			// wynik w centymetrach
 	double 	przelicznik = 2.54;		// przelicznik: cale na centymetry
+	string cal;
 
-	std::cout << "Podaj wielosc w calach: ";
-	std::cin >> cale;				// przyjecie danych w calach
+	cout << "Podaj wielosc w calach: ";
+	cin >> cale;				// przyjecie danych w calach
 
 	centymetry = cale * przelicznik;	// przeliczenie
 
-	std::cout << "\n";
+	cout << endl;
 
 	// wypisanie wynikow
-	std::cout << cale << " (w calach) - jest rownoznaczne z: " << centymetry << " (w centymetrach)\n";
+	//cout << cale << " (w calach) - jest rownoznaczne z: " << centymetry << " (w centymetrach)\n";
+
+	if(cale == 1) cal = "cal";
+	else if(cale >=2 && cale <= 4) cal = "cale";
+	else if(cale >=5 && cale <= 21) cal = "cali";
+	else if(cale % 10 >=2 && cale % 10 <= 4) cal = "cale";
+	else cal = "cali";
+
+	cout << cale << " " << cal << " - to: " << centymetry << " cm\n";
 }
